2e587c3d0fe0542608726b5b4032e0ff73eb76b7
joao gomes <joao_rgomes@hotmail.com>
2022-05-25 08:33:57 -0300

    commit made after working fetchs from the API

--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -1,15 +1,27 @@
-import { render } from '@testing-library/react';
 import React from 'react';
 import './Playlist.css';
 import TrackList from '../TrackList/TrackList';
 
 class Playlist extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.handleNameChange = this.handleNameChange.bind(this);
+    }
+
+    handleNameChange(e) {
+        this.props.onNameChange(e.target.value);
+    }
     render() {
-        return <div className="Playlist">
-            <input value="New Playlist"/>
-            <TrackList />
-            <button className="Playlist-save">SAVE TO SPOTIFY</button>
+        return (
+        <div className="Playlist">
+            <input defaultValue={"New Playlist"} onChange={this.handleNameChange}/>
+              <TrackList tracks={this.props.playlistTracks} 
+                         onRemove={this.props.onRemove} 
+                         isRemoval={true}/>  
+            <button className="Playlist-save" onClick={this.props.onSave}>SAVE TO SPOTIFY</button>
         </div>
+        )
     }
 }
 
--- src/components/TrackList/TrackList.js
+++ src/components/TrackList/TrackList.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import './TrackList';
+import './TrackList.css';
+import Track from '../Track/Track';
 
 class TrackList extends React.Component {
     render() {
         return (
-        <div classNAme="TrackList">
-            <p>three tracks</p>
+        <div className="TrackList">
+            {       
+                    this.props.tracks.map((track) => {
+                    return <Track track={track} 
+                                 key={track.id} 
+                                 onAdd={this.props.onAdd} 
+                                 onRemove={this.props.onRemove} 
+                                 isRemoval={this.props.isRemoval} /> 
+                })
+            }  
         </div>
         )
     }
@@ -13,4 +22,3 @@ class TrackList extends React.Component {
 
 export default TrackList;
 
-//<!-- You will add a map method that renders a set of Track components  -->
